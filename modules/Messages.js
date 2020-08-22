58cc031c0a1d414d04e81e9f285ef1715480bc9a
Joel Lee <lywjoel@gmail.com>
2020-08-22 22:48:10 +0800

    welcome message updates

--- modules/Messages.js
+++ modules/Messages.js
@@ -18,13 +18,12 @@ module.exports = {
     getWelcomeMessage: (name) => {
         return `What's up ${name}!!` + 
         "\n\nMy name is M.A.V.I.S., short for Machine-Assisted Velocity Information Service! I'm here to provide a few services:\n\n" + 
-        "1) First off, I'll be sending you the daily passages that we're reading together as a ministry.\n" +
-        "2) If you don't want to receive the daily passages, use the '/unsubscribe' command. If you change your mind after, you can always '/subscribe' again!\n" + 
-        "3) To get the latest passage(s) for the day, use the '/latest' command.\n" + 
-        "4) You can submit a testimony to shout the name of Jesus by using the '/shouthisname' command!\n" + 
-        "5) I'll also be sending you any announcements made by admins, and any approved testimonies from other users!\n" + 
-        "6) Want to hop on to our service livestreams? Use '/livestream' to get the link!\n" +
-        "7) Lastly, you can also send me feedback with the '/feedback' command.\n\n" + 
+        "1) First off, I'll be sending you the daily verses that we're reading together as a ministry. Let's grow together in the word of God!\n" +
+        "2) To get the latest passage(s) for the day, use the '/latest' command.\n" + 
+        "3) You can submit a testimony to shout the name of Jesus by using the '/shouthisname' command!\n" + 
+        "4) Want to hop on to our Sunday online service livestreams? Use '/livestream' to get the link!\n" +
+        "5) We'd love to hear from you! You can send me feedback with the '/feedback' command.\n" + 
+        "6) Lastly, if you don't want to receive the daily passages, use the '/unsubscribe' command. If you change your mind after, you can always '/subscribe' again!\n\n" + 
         "If you ever need a recap, you can always ask me for '/help'!";
     },
     
