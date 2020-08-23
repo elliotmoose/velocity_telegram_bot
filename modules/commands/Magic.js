f641876bee5d95f3affd1d80cade4702934adb2e
Joel Lee <lywjoel@gmail.com>
2020-08-23 21:51:41 +0800

    /magic to update latest verses as well

--- modules/commands/Magic.js
+++ modules/commands/Magic.js
@@ -7,6 +7,7 @@ module.exports = async (message, storage, broadcaster) => {
     const id = message.from.id;
     if (storage.userStorage.isUserAdmin(id)) {
         await storage.userStorage.pullUsersToCache();
+        await storage.verseStorage.pullLatestVersesToCache();
         await broadcaster.sendMessage(id, Messages.magicMessage);
     } else {
         await broadcaster.sendMessage(id, Messages.nonMagicianMessage);
